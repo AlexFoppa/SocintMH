7e28cd721901b1d4c32501c7c44c4e36c52259a2
AlexFoppa <alfoppa@hotmail.com>
2023-07-14 21:54:32 -0300

    clean demonstration call

--- index.py
+++ index.py
@@ -13,7 +13,7 @@ clean_table_person()
 clean_tables_instagram()
 
 #Este método foi mantido no index para facilitar a demonstração nesta versão
-#Futuramente, a ideia é criar um arquivo getInstagramData.py na camada processos. Não sei se manterei o form - se sim, creio que dividiria entre presentation and process (necessário avaliar) 
+#Futuramente, a ideia é criar um arquivo na camada processos. Não sei se manterei o form - se sim, creio que dividiria entre presentation and process (necessário avaliar) 
 
 if 'submit' in form:
     name = form.getvalue('name')
@@ -25,22 +25,13 @@ if 'submit' in form:
 else:
     name = 'Alexandre'
     doc = '123456'
-    insta_auth_code = 'AQDUpCKLc9YCzTZ8tTZg30OYWJAjhy9Yc78ioRcnHd5lCRTlIy26-cccUFA7G0xOXpSaP60n_KsCkBy3CUhqElqPEHLkpn7L67QKaOBgfE4RuvW8fsEHH7tHsEdOoMc1yAUy_DAaSPs3h-2YP_Hf0JfmuVPVSurYuLToN8-c2KFmg5s9t5lO4WCpr-RPx9yp08StCxHMs_TJq57aL0Ko55CidwIrpiM6Vo3zfymR1Zo6-A'
+    insta_auth_code = ''
     history = 'sintoma 1 e 2'
     email = 'oi@gmail.com'
 
 person = Person(name=name,doc=doc,email=email,history=history)
 personId=insert_person(person)
-
-print('-----------------1 - Pessoa cadastrada no BD ----------------------------')
-print(get_all_persons())
-
 user_id, access_token = get_insta_id(insta_auth_code)
-
-print('------------------2 - Retorno da API do Instagram ---------------------------')
-print('Person ID:',personId)
-print('User ID:',user_id)
-print('Access Token:',access_token)
 
 insta_profile = get_insta_profile(user_id, access_token)   
 instaTwin = InstagramTwin(
@@ -51,11 +42,8 @@ instaTwin = InstagramTwin(
     username=insta_profile['username'])
 insert_twin(instaTwin)
 
-print('----------------3 - Digital Twin: Perfil do Instagram no BD -----------------------------')
-print(get_all_twins())
-
 insta_publication= get_insta_media(access_token)
-print('----------------------4 - Análise de Sentimento das Postagens ----------------------')
+
 for pub in insta_publication["data"]:
     feed_publication = InstagramFeedPublication(
         personId=personId,
@@ -68,17 +56,5 @@ for pub in insta_publication["data"]:
         timestamp = pub["timestamp"],
         username = pub["username"]
         )
-    print(get_sentiment_polarity(pub["caption"]),": ",pub["caption"])
     insert_publication(feed_publication)
-
-print('--------------------5 - Digital Twin: Publicações do Feed no BD -------------------------')     
-print(get_all_pubs())
-print('--------------------6 - Consulta aos dados da pessoa -------------------------')     
-
-person = get_person_by_name('Alexandre')[0]
-person = Person(id=person[0],name=person[1],doc=person[2],email=person[3],history=person[4])
-
-print('Pede os dados da pessoa sem argumento:', person.get_person_data())
-print('Pede os dados da pessoa pedindo pelo Instagram', person.get_person_data('Instagram'))
-print('Pede os dados da pessoa pedindo pelo Facebook', person.get_person_data('Facebook'))
-print('Pede todos os dados da pessoa',person.get_person_and_twins_data())
+   
